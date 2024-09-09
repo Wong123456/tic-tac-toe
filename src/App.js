30dd969ce986bc72890acd2be57d742f6378993e
Wong123456 <wong.weixiang181103200302@gmail.com>
2024-09-09 17:24:30 +0800

    game logic done, included win determination, starting on css styling

--- src/App.js
+++ src/App.js
@@ -12,23 +12,34 @@ function App() {
   const [won, setWon] = useState(false);
   const [winner, setWinner] = useState("");
   const [size, setSize] = useState(3);
+  const [board, setBoard] = useState(emptyBoard());
+
+  function emptyBoard(){
+    return [...Array(3)].map(() => Array(3).fill("null"));
+  }
+
+  function updateBoard(newBoard){
+    setBoard([...newBoard]);
+  }
 
   function handleTurn(){
     setTurn(turn + 1);
     setNewGame(false);
-    // scan(3);
-    if (!won){
-      handleAnnouncement(turn + 1);
-    }
-    else {
-      handleAnnouncement(100);
-    }
+    handleAnnouncement(turn + 1);
 
+    // scanBoard();
+    // if (!won) {
+    //   handleAnnouncement(turn + 1);
+    // }
+    // else{
+    //   handleAnnouncement(100);
+    // }
   }
 
   function restartTurn(){
-    setTurn(0);
-    handleAnnouncement(0);
+    setTurn(1);
+    setBoard(emptyBoard());
+    handleAnnouncement(1);
     setNewGame(true);
     setWon(false);
   }
@@ -36,7 +47,7 @@ function App() {
   function handleAnnouncement(turn){
     let mark = turn % 2 == 0 ? "X" : "O";
     let string = "";
-    if (turn == 100) {string = "Game end, " + winner + "wins!";}
+    if (turn == 100) {string = "Game end, " + winner + " wins!";}
     else{
       if (turn <= 9) {
         string = "Turn " + turn + ", " + mark + "'s move";
@@ -48,61 +59,92 @@ function App() {
     setAnnouncement(string);
   }
 
-  function scan(size){
-    let marks = []
-    for (let i = 1; i <= size^2; i++){
-      marks.push(document.getElementById(i).value);
-    }
-    if ((marks[0] == marks[1] == marks[2]) && (marks[0] != "")) {setWon(true); setWinner(marks[0]);}
-    if (marks[3] == marks[4] == marks[5]) {setWon(true); setWinner(marks[3]);}
-    if (marks[6] == marks[7] == marks[8]) {setWon(true); setWinner(marks[6]);}
-    if (marks[0] == marks[3] == marks[6]) {setWon(true); setWinner(marks[0]);}
-    if (marks[1] == marks[4] == marks[7]) {setWon(true); setWinner(marks[1]);}
-    if (marks[2] == marks[5] == marks[8]) {setWon(true); setWinner(marks[2]);}
-    if (marks[0] == marks[4] == marks[8]) {setWon(true); setWinner(marks[0]);}
-    if (marks[2] == marks[4] == marks[6]) {setWon(true); setWinner(marks[2]);}
+  function scanBoard(){
+    if ((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && (board[0][0] != "null")) {setWon(true); setWinner(board[0][0]);}
+    if ((board[1][0] == board[1][1] && board[1][1] == board[1][2]) && (board[1][0] != "null")) {setWon(true); setWinner(board[1][0]);}
+    if ((board[2][0] == board[2][1] && board[2][1] == board[2][2]) && (board[2][0] != "null")) {setWon(true); setWinner(board[2][0]);}
+    if ((board[0][0] == board[1][0] && board[1][0] == board[2][0]) && (board[0][0] != "null")) {setWon(true); setWinner(board[0][0]);}
+    if ((board[0][1] == board[1][1] && board[1][1] == board[2][1]) && (board[0][1] != "null")) {setWon(true); setWinner(board[0][1]);}
+    if ((board[0][2] == board[1][2] && board[1][2] == board[2][2]) && (board[0][2] != "null")) {setWon(true); setWinner(board[0][2]);}
+    if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && (board[0][0] != "null")) {setWon(true); setWinner(board[0][0]);}
+    if ((board[2][0] == board[1][1] && board[1][1] == board[0][2]) && (board[2][0] != "null")) {setWon(true); setWinner(board[2][0]);}
   }
 
-  // useEffect(() =>{
-  //   scan();
-  // }, [turn])
+  useEffect(() =>{
+    setBoard(board);
+  }, [board])
 
+  useEffect(() => {
+    if (turn > 4) {scanBoard();}
+  }, [turn])
+
+  useEffect(() => {
+    if (won){
+      handleAnnouncement(100);
+      // setWon(false);
+      console.log("global won: ");
+    }
+  }, [won])
+ 
   return (
     <div className="App">
       <h1>{announcement}</h1>
       <h2>{newGame}</h2>
-      <FullBoard turn={turn} handleTurn={handleTurn} newGame={newGame} size={size}/>
+      <FullBoard turn={turn} handleTurn={handleTurn} newGame={newGame} size={size} boardState={board} updateBoard={updateBoard} won={won}/>
       <RestartButton restartTurn={restartTurn}/>
     </div>
   );
 }
 
-function Square({turn, handleTurn, newGame, id}){
-  const [mark, setMark] = useState("");
+function Cross(){
+  return(
+    <h3 className="cross">
+      X
+    </h3>
+  );
+}
+
+function Circle(){
+  return(
+    <h3 className="circle">
+      O
+    </h3>
+  );
+}
+
+function Square({turn, handleTurn, newGame, id, row, col, boardState, updateBoard, won}){
+  const [mark, setMark] = useState("null");
   const [isMarked, setIsMarked] = useState(false);
+
+  let board = boardState;
 
   useEffect(() =>{
     if (newGame){
-    setMark("");
+    setMark("null");
     setIsMarked(false);
     }
   }, [newGame])
 
-  function clearMark(){
-    setMark("");
-    setIsMarked(false);
-  }
-
   function handleMark(){
-    if (!isMarked){
-      if (turn %2 === 0) {setMark("X");}
-      else {setMark("O");}
+    console.log(row, col);
+
+    if (!isMarked && !won){
+      if(won) {console.log("won");}
+      else if (!won) {console.log("not won");}
+      if (turn %2 === 0) {
+        setMark(Cross());
+        board[row][col] = "X";
+      }
+      else {
+        setMark(Circle());
+        board[row][col] = "O";
+      }
+
       handleTurn();
       setIsMarked(true);
-      console.log(id + ", " + mark);
+      updateBoard(board);
     }
   }
-
 
   return(
     <>
@@ -111,47 +153,40 @@ function Square({turn, handleTurn, newGame, id}){
   );
 }
 
-function BoardRow({turn, handleTurn, newGame, size, rowNum}){
-  let row = [];
+function BoardRow({turn, handleTurn, newGame, size, row, boardState, updateBoard, won}){
+  let squares = [];
+  let col = 0;
   for (let i = 1; i <= size; i++) {
     let id = 0;
-    if (rowNum == 1){
+    if (row == 0){
       id = i;
     }
-    else if (rowNum == 2){
+    else if (row == 1){
       id = i + size;
     }
-    else if (rowNum == 3){
+    else if (row == 2){
       id = i + (size * 2);
     }
-    row.push(Square({turn, handleTurn, newGame, id}));
+    col = i - 1;
+    squares.push(Square({turn, handleTurn, newGame, id, row, col, boardState, updateBoard, won}));
   }
-  return <div>{row}</div>
+  return <div>{squares}</div>
 }
 
-function FullBoard({turn, handleTurn, newGame, size}){
+function FullBoard({turn, handleTurn, newGame, size, boardState, updateBoard, won}){
   let rows = [];
-  let rowNum = 1;
-  while (rowNum <= size) {
-    rows.push(BoardRow({turn, handleTurn, newGame, size, rowNum}));
-    rowNum++;
+  let row = 0;
+  while (row <= size-1) {
+    rows.push(BoardRow({turn, handleTurn, newGame, size, row, boardState, updateBoard, won}));
+    row++;
   }
-  return rows;
+  return rows;          
 }
 
 function RestartButton({restartTurn}){
 
-  function restart(){
-    const squares = document.getElementsByClassName("square");
-    for (let square of squares){
-      square.innerText = "";
-    }
-    // document.getElementById("square").innerText = "";
-    restartTurn();
-  }
-
   return(
-    <button className="restartButton" onClick={restart}>Restart</button>
+    <button className="restartButton" onClick={restartTurn}>Restart</button>
   );
 
 }
